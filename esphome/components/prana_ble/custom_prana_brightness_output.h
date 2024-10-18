bbf68bc1a3ab32a391721f904fcc7f7655c3ed6d
Vladislav <3158402+vpritulin@users.noreply.github.com>
2024-10-19 00:12:44 +0300

    fixed

--- esphome/components/prana_ble/custom_prana_brightness_output.h
+++ esphome/components/prana_ble/custom_prana_brightness_output.h
@@ -1,4 +1,5 @@
 #include "esphome/core/component.h"
+#include "esphome/core/log.h"  // Правильное подключение для логирования
 #include "esphome/components/output/float_output.h"
 #include "esphome/components/prana_ble/prana_ble_hub.h"
 
@@ -7,9 +8,8 @@ class CustomPranaBrightnessOutput : public esphome::Component, public esphome::o
   CustomPranaBrightnessOutput(esphome::prana_ble::PranaBLEHub *hub) : hub_(hub) {}
 
   void write_state(float state) override {
-    // Convert the input value (0.0 - 1.0) to the range from 1 to 6
     int brightness_value = static_cast<int>(state * 5) + 1;
-    ESP_LOGD("custom_brightness", "Setting brightness to: %d", brightness_value);
+    ESP_LOGD("custom_brightness", "Setting brightness to: %d", brightness_value); 
     hub_->set_brightness(brightness_value);
   }
 
