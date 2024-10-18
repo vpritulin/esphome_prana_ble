25c4cbef7a8ea84c8feb7c3725566d171fed7414
Vladislav <3158402+vpritulin@users.noreply.github.com>
2024-10-18 23:59:12 +0300

    updated

--- custom_light_output.h
+++ custom_light_output.h
@@ -8,18 +8,17 @@
 namespace esphome {
 namespace custom_light {
 
-class CustomLightOutput : public Component, public light::LightOutput {
+class CustomLightOutput : public esphome::Component, public esphome::light::LightOutput {
  public:
-   CustomLightOutput(std::function<void(float)> write_state_func) : write_state_func_(write_state_func) {}
+  CustomLightOutput(std::function<void(float)> write_state_func) : write_state_func_(write_state_func) {}
 
-   void write_state(float state) override {
-
-     ESP_LOGD("custom_light", "Setting brightness to: %.2f", state);
-     this->write_state_func_(state);
-   }
+  void write_state(float state) override {
+    printf("Setting brightness to: %.2f\n", state);
+    this->write_state_func_(state);
+  }
 
  private:
-   std::function<void(float)> write_state_func_;
+  std::function<void(float)> write_state_func_;
 };
 
 }  // namespace custom_light
