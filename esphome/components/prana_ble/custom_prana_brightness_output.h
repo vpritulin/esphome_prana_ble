116fc791046360b6268483aae8682d9062fb81f3
Vladislav <3158402+vpritulin@users.noreply.github.com>
2024-10-18 23:47:33 +0300

    fixed problem

--- esphome/components/prana_ble/custom_prana_brightness_output.h
+++ esphome/components/prana_ble/custom_prana_brightness_output.h
@@ -1,10 +1,18 @@
-#include "esphome.h"
+#include "esphome/core/component.h"
+#include "esphome/components/output/float_output.h"
+#include "esphome/components/prana_ble/prana_ble_hub.h"
 
-class CustomPranaBrightnessOutput : public Component, public FloatOutput {
+class CustomPranaBrightnessOutput : public esphome::Component, public esphome::output::FloatOutput {
  public:
+  CustomPranaBrightnessOutput(esphome::prana_ble::PranaBLEHub *hub) : hub_(hub) {}
+
   void write_state(float state) override {
     // Convert the input value (0.0 - 1.0) to the range from 1 to 6
-    int brightness_value = (int)(state * 5) + 1;
-    id(prana)->set_brightness(brightness_value);
+    int brightness_value = static_cast<int>(state * 5) + 1;
+    ESP_LOGD("custom_brightness", "Setting brightness to: %d", brightness_value);
+    hub_->set_brightness(brightness_value);
   }
+
+ protected:
+  esphome::prana_ble::PranaBLEHub *hub_;
 };
